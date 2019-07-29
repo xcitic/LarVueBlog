5d5b6b72515250f73c4f2a0ddab5760d4597410f
xcitic <sami@ecompx.com>
2019-07-29 19:26:05 +0200

    BlogPost basic logic setup

---
+++ backend/app/BlogPost.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BlogPost extends Model
+{
+    protected $table = 'blog_posts';
+    protected $fillable = [
+      'title',
+      'description',
+      'content',
+      'image',
+      'likes',
+      'views'
+    ];
+
+    public function owner() {
+      return $this->belongsTo('App\User', 'user_id');
+    }
+
+    public function comments() {
+      return $this->hasMany('App\Comment');
+    }
+
+}
---
+++ backend/database/migrations/2019_07_29_171904_create_blog_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlogPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('description');
+            $table->text('content');
+            $table->string('image');
+            $table->integer('likes');
+            $table->integer('views');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_posts');
+    }
+}
