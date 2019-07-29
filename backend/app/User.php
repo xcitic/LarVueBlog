355edf46a010c3bc413a65bbaeebcb180244ff45
xcitic <sami@ecompx.com>
2019-07-29 19:09:19 +0200

    Users have a role (user / admin)

--- backend/app/User.php
+++ backend/app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'role'
     ];
 
     /**
@@ -37,4 +37,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function isAdmin() {
+      if ($this->role === 'admin') {
+        return true;
+      }
+      return false;
+
+    }
 }
