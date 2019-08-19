72ba6166c1dafb697fb6b8b33e7684d0f5e71b65
xcitic <xcitic@gmail.com>
2019-08-19 12:29:48 +0200

    Setup script

--- setup.sh
+++ setup.sh
@@ -3,30 +3,37 @@
 ## Author: Sami
 ## License: MIT
 
-# Not complete
-
-# Get current user
-declare user=$(id -u -n)
-echo $user
 
 # Setup project dependencies
 composer install
 yarn install
 
+# Setting up env variables. Default PDO is Sqlite
+cp .env.example .env
+
+# generate application salt
+php artisan key:generate
 # Build production app.js
 yarn run prod
-
-# Setup database
+# setup database file sqlite
+touch database/database.sqlite
 php artisan migrate
+# Setup passport keys
 php artisan passport:install
+# Seed with dummy data
 php artisan db:seed
 
 # Setup permissions
 echo 'Is the website on a server? (y/n)'
 read server
 
-if [$server -eq 'y']; then
-  sudo chmod -R $user:www-data storage/
+if ["$server" == 'y']; then
+  # Get current user
+  declare user=$(id -u -n)
+  sudo chown -R $user:www-data storage/{app,framework,logs} bootstrap/cache
+  sudo chown -R www-data:www-data public/images
 else
-  echo 'Complete'
+  echo 'Setup complete'
+  echo 'Starting dev server'
+  php artisan serve --port 8000
 fi
